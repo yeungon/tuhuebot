1ad08217e4770af20e03f230d2a3d5920e81ddd1
vuong <yeungon@gmail.com>
2024-09-09 15:30:33 +0700

    First commit

---
+++ cmd.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	tele "gopkg.in/telebot.v3"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	pref := tele.Settings{
+		Token:  os.Getenv("TOKEN"),
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	}
+
+	//fmt.Println("Hello, World!")
+
+	b, err := tele.NewBot(pref)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	b.Handle("/start", func(c tele.Context) error {
+		fmt.Println(c.Text())
+		return c.Send("Hello, how are you")
+	})
+
+	b.Start()
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/yeungon/tuhuebot
+
+go 1.22.3
+
+require (
+	github.com/joho/godotenv v1.5.1
+	gopkg.in/telebot.v3 v3.3.8 // indirect
+)
