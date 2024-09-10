74745544bea36cd3aeafaa2edbb68e7178cd4890
vuong <yeungon@gmail.com>
2024-09-10 15:03:33 +0700

    Update text

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 run: build
 	@ ./bin/tuhuebot start
 build:
-	@go build -o bin/tuhuebot
+	@go build -o bin/tuhuebot -buildvcs=false
 dev:
 	go run main.go
 start:
 	sudo systemctl start tuhuebot
+stop:
+	sudo systemctl stop tuhuebot
 restart:
 	sudo systemctl restart tuhuebot
 status:
--- main.go
+++ main.go
@@ -30,27 +30,22 @@ func main() {
 	fmt.Println("Hi, your bot is running as expected!")
 
 	b.Handle("/start", func(c tele.Context) error {
-		fmt.Println(c.Text())
 		return c.Send("Hi, chào mừng bạn đến với bot trợ lý học tập KTU. Chúc bạn một ngày tốt lành. Xin lựa chọn tác vụ phía dưới")
 	})
 
 	b.Handle("/about", func(c tele.Context) error {
-		fmt.Println(c.Text())
-		return c.Send("Đây là bot hỗ trợ tự động")
+		return c.Send("Đây là phần mềm dạng bot hỗ trợ tự động, giúp sinh viên tiếp cận thông tin học tập nhanh và thuận lợi hơn. Thông tin mang giá trị tham khảo. Hãy liên hệ trực tiếp với giảng viên hướng dẫn hoặc cố vấn học tập để có được trợ giúp chi tiết hơn!")
 	})
 
 	b.Handle("/qa", func(c tele.Context) error {
-		fmt.Println(c.Text())
 		return c.Send("Hỏi đáp")
 	})
 
 	b.Handle("/guide", func(c tele.Context) error {
-		fmt.Println(c.Text())
 		return c.Send("Hướng dẫn")
 	})
 
 	b.Handle("/help", func(c tele.Context) error {
-		fmt.Println(c.Text())
 		return c.Send("Các gợi ý giúp bạn sử dụng bot tốt hơn")
 	})
 
